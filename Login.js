04de79ce88fdd7324f4040d5a2224a0942756cd2
Singuluri Padmini Devi <149938777+Singuluri-Padmini-Devi@users.noreply.github.com>
2024-07-21 11:13:04 +0530

    Login.js

---
+++ Login.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const Login = ({ setToken }) => {
+  const [formData, setFormData] = useState({ username: '', password: '' });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/api/users/login', formData);
+      setToken(response.data.token);
+    } catch (error) {
+      alert('Error logging in');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="username" placeholder="Username" onChange={handleChange} />
+      <input type="password" name="password" placeholder="Password" onChange={handleChange} />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
